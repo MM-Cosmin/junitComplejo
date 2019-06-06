1e9a712f1bf5c545bfb74f64491aaf4482c66240
Cosmin <Cosmin@DESKTOP-SHR60V6>
2019-06-06 16:23:41 +0200

    Restar

--- junitComplejo/test/junitComplejo/ComplejoTest2.java
+++ junitComplejo/test/junitComplejo/ComplejoTest2.java
@@ -7,10 +7,10 @@ import org.junit.Test;
 
 public class ComplejoTest2 {
 	Complejo c1, c2;
-	
+
 	@Before
 	public void setUp() throws Exception {
-        // 1. Arrange 
+        // 1. Arrange
         c1 = new Complejo(3, 5);
         c2 = new Complejo(1, -1);
 
@@ -18,13 +18,13 @@ public class ComplejoTest2 {
 
 	@Test
 	public void testRestar() {
-		
+
 		Complejo resultado = c1.restar(c2);  // 2. Act
-		
+
 		//3. Assert
 		assertEquals(resultado.getParteReal(), 2, 0.0000001);
 		assertEquals(resultado.getParteImaginaria(), 6, 0.0000001);
-		
+
 	}
 
 }
